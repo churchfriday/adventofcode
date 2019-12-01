f37042122623ed795adf1d90bd1c72843171e7f2
churchfriday <phusioo@yahoo.com>
2019-12-01 17:07:07 +0100

    day1 over

--- day1-advent-of-code.py
+++ day1-advent-of-code.py
@@ -1,10 +1,31 @@
 fuel = 0
 flag = "true"
-input_mass = int(input())
+#input_mass = int(input())
 mass = 0
-while input_mass != 0:
+sumofallmodules = 0
+currentmodule = 0
+sum_of_fuel_current = 0
 
-    fuel+=(int(((input_mass/3)-2)))
-    print(fuel)
-    input_mass = int(input())
-print(fuel)
+filepath = 'numbers.txt'
+try:  
+    with open(filepath) as fp:
+        line = fp.readline()
+        while line.isdigit:
+            line = fp.readline()      
+            line1 = int(line)
+            fuel_current=int(line1/3)-int(2)
+            #fuel+=fuel_current
+            print ("fuel current: {}".format(fuel_current))
+
+            while(int(fuel_current/3)>0):
+                #fuel_current=int(fuel_current/3)-int(2)
+                sum_of_fuel_current+=fuel_current
+                fuel_current=int(fuel_current/3)-int(2)
+                print ("fuel current: {}".format(fuel_current))
+            if((int(fuel_current/3)-2<0)and(fuel_current > 0)):
+                sum_of_fuel_current+=fuel_current
+                continue
+except:
+    print("overall fuel {}".format(sum_of_fuel_current))
+finally:
+    fp.close()
